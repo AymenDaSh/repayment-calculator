6ea4d53e569abcf6b29c68c7c83fa00f14187542
AymenDaSh <dgoldmanb@gmail.com>
2024-09-03 13:07:23 -0400

    handling error , styling , and remove/add error classes using js

--- script/calculate.js
+++ script/calculate.js
@@ -52,7 +52,7 @@ document.querySelector(".js-clear-link").addEventListener("click", () => {
         </p>
   `;
   resultContainerHTML.innerHTML = resultHTML;
-  resultContainer.classList.add("result-container-empty");    
+  resultContainerHTML.classList.add("result-container-empty");    
 })
 }
 
@@ -63,12 +63,62 @@ function calculation(){
   const mortgageTerm = mortgageTermHTML.value;
   const mortgageRate = mortgageRateHTML.value;
   
+  //handling errors
+
+  errorHandling();
+  
 
   if(checkboxRepayment.classList.contains("checkmark-checked")){
     // the formule is incorect just for testing
+    document.querySelector(".error-type").classList.remove("error-activated")
     return (mortgageAmount + mortgageTerm) * mortgageRate;
   }else if(checkboxInterestOnly.classList.contains("checkmark-checked")){
+    document.querySelector(".error-type").classList.remove("error-activated")
     return 19223.34;
+  }
+}
+
+
+
+function errorHandling(){
+  const mortgageAmount = mortgageAmountHTML.value;
+  const mortgageTerm = mortgageTermHTML.value;
+  const mortgageRate = mortgageRateHTML.value;
+  const checkboxRepayment = document.querySelector(".checkbox-repayments");
+  const checkboxInterestOnly = document.querySelector(".checkbox-interest-only");
+
+  if(!checkboxRepayment.classList.contains("checkmark-checked") && 
+     !checkboxInterestOnly.classList.contains("checkmark-checked")){
+   document.querySelector(".error-type").classList.add("error-activated")
+  }
+
+  if(mortgageAmount === ''){
+    mortgageAmountHTML.classList.add("error-input");
+    document.querySelector(".currency").classList.add("error-signe");
+    document.querySelector(".error-amount").classList.add("error-activated");
+  }else{
+    mortgageAmountHTML.classList.remove("error-input");
+    document.querySelector(".currency").classList.remove("error-signe");
+    document.querySelector(".error-amount").classList.remove("error-activated");
+  }
+
+  if(mortgageTerm === ''){
+    mortgageTermHTML.classList.add("error-input");
+    document.querySelector(".years").classList.add("error-signe");
+    document.querySelector(".error-term").classList.add("error-activated");
+  }else{
+    mortgageTermHTML.classList.remove("error-input");
+    document.querySelector(".years").classList.remove("error-signe");
+    document.querySelector(".error-term").classList.remove("error-activated");
+  }
+  if(mortgageRate === ''){
+    mortgageRateHTML.classList.add("error-input");
+    document.querySelector(".rate").classList.add("error-signe");
+    document.querySelector(".error-rate").classList.add("error-activated");
+  }else{
+    mortgageRateHTML.classList.remove("error-input");
+    document.querySelector(".rate").classList.remove("error-signe");
+    document.querySelector(".error-rate").classList.remove("error-activated");
   }
 }
 
